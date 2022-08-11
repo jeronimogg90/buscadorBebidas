5087c00b210a00697b7de677283e6d1aa33749fa
Jerónimo García García <jeronimogga@ext.inditex.com>
2022-08-11 09:28:47 +0200

    Proyecto Entero

---
+++ README.md
@@ -0,0 +1,1 @@
+# Este proyecto esta hecho en react con Context API y react-bootstrap, consumimos una API con axios, tenemos 2 Providers direferentes y mostramos los resultados en una modal
---
+++ src/components/Bebida.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Card, Col, Button } from 'react-bootstrap'
+import useBebidas from '../hooks/useBebidas'
+
+const Bebida = ({bebida}) => {
+
+    const { handleModalClick, handleBebidaIdClick } = useBebidas()
+
+  return (
+    <Col md={6} lg={3}>
+        <Card className='mb-4'>
+            <Card.Img
+                variant="top"
+                src={bebida.strDrinkThumb}
+                alt={`Imagen de ${bebida.strDring}`}
+            />
+        </Card>
+
+        <Card.Body>
+            <Card.Title>
+                {bebida.strDrink}
+            </Card.Title>
+            <Button
+                className="w-100 text-uppercase mt-2"
+                variant="warning"
+                onClick={() => {
+                    handleModalClick()
+                    handleBebidaIdClick(bebida.idDrink)
+                }}
+            >
+                Ver Receta
+            </Button>
+        </Card.Body>
+    </Col>
+  )
+}
+
+export default Bebida
